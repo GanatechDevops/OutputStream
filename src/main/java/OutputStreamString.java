afa9d8afada7d7cbc531ae2da243ddb176fcec8e
rasool <rasools27@gmail.com>
2018-09-05 03:05:35 -0700

    add file

---
+++ src/main/java/OutputStreamString.java
@@ -0,0 +1,15 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class OutputStreamString {
+	public static void main(String[] args) throws IOException {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String line = "Hello there!";
+
+        stream.write(line.getBytes());
+        String finalString = new String(stream.toByteArray());
+
+        System.out.println(finalString);
+    }
+}
